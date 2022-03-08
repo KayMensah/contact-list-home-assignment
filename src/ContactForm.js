29d84bc95c4f2f74413f934dc8e3e410e079e06c
Korkor Mensah <komensah2018@gmail.com>
2022-03-08 15:41:03 +0000

    added ccs style

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,34 +1,73 @@
 import React, { useState } from "react";
+import { Divider } from "semantic-ui-react";
 
 const ContactForm = (props) => {
-    const [name, setName] = useState("");
-    const [phoneNumber, setphoneNumber] = useState("");
-    const [location, setLocation] = useState("");
+  const [name, setName] = useState("");
+  const [phoneNumber, setphoneNumber] = useState("");
+  const [location, setLocation] = useState("");
 
-    return (
-        <form
-            onSubmit={(e) => {
-                console.log('Hit here')
-                e.preventDefault();
+  return (
+    <form
+      className="form-style"
+      onSubmit={(e) => {
+        console.log("Hit here");
+        e.preventDefault();
 
-            let newContact ={
-                name: name,
-                phoneNumber: phoneNumber,
-                location: location,
-            };
-            props.addContact(newContact);
-            setName("");
-            setphoneNumber("");
-            setLocation("");
-            }}>
+        let newContact = {
+          name: name,
+          phoneNumber: phoneNumber,
+          location: location,
+        };
+        props.addContact(newContact);
+        setName("");
+        setphoneNumber("");
+        setLocation("");
+      }}
+    >
+      <h2 className="style-direction">React Redux Users</h2>
+      <div className="form-row">
+        <div className="col-md-6 mb-3">
+          <h3 className="style-direction">CONTACTS</h3>
+          <label>Name:</label>
+          <input
+            className="style1"
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="form-row">
+        <div className="col-md-6 mb-3">
+          <label>Phone Number:</label>
+          <input
+            className="style3"
+            type="number"
+            placeholder="PhoneNumber"
+            value={phoneNumber}
+            onChange={(e) => setphoneNumber(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="form-row">
+        <div className="col-md-6 mb-3">
+          <label>Location:</label>
+          <input
+            className="style2"
+            type="text"
+            placeholder="Location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </div>
+      </div>
 
-            <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
-            <input type="number" placeholder="PhoneNumber" value={phoneNumber} onChange={(e) => setphoneNumber(e.target.value)}/> 
-            <input type="text" placeholder="Location" value={location} onChange={(e) => setLocation(e.target.value)}/>
-
-            <button type="submit">Add contact</button>
-        </form>
-    )
-} 
+      <button className="button-style" type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+};
 
 export default ContactForm;
--- src/ContactList.js
+++ src/ContactList.js
@@ -1,21 +1,19 @@
 import React from "react";
 
 const ContactList = (props) => {
-    return (
-        <div>
-            {props.contacts.map((contact, index) => {
-
-                return (
-                    <div key={index}>
-                        <h1>{contact.name}</h1>
-                        <h3>{contact.phoneNumber}</h3>
-                        <h3>{contact.location}</h3>
-
-                    </div>
-                );
-            })}
-        </div>
-    );
+  return (
+    <div>
+      {props.contacts.map((contact, index) => {
+        return (
+          <div className="list-style" key={index}>
+            <h2>{contact.name}</h2>
+            <h3>{contact.phoneNumber}</h3>
+            <h3>{contact.location}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ContactList;
